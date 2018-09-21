a3c0e9f600dc10e1cadddd073e692f82c64e0550
Pedro Vázquez Machado <pedro.vazquez.machado@everis.com>
2018-09-21 13:31:05 +0200

    My changes...

--- app/phone-list/phone-list.component.spec.js
+++ app/phone-list/phone-list.component.spec.js
@@ -21,9 +21,14 @@ describe('phoneList', function() {
     }));
 
     it('should create a `phones` property with 2 phones fetched with `$http`', function() {
+      // we will make assertions to verify that the phones property doesn't exist on the controller before the response is received
       expect(ctrl.phones).toBeUndefined();
 
+      // We flush the request queue in the browser by calling $httpBackend.flush().
+      // This causes the promise returned by the $http service to be resolved with the trained response.
+      // See Flushing HTTP requests in the mock $httpBackend documentation for a full explanation of why this is necessary
       $httpBackend.flush();
+
       expect(ctrl.phones).toEqual([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
     });
 
