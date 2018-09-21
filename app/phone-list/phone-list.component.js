737c125ca3d332e5149c6f39f18efe09d6b767df
Pedro Vázquez Machado <pedro.vazquez.machado@everis.com>
2018-09-21 12:55:47 +0200

    Solving problems with minification. Option 1)...

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -1,16 +1,20 @@
 'use strict';
 
-// Register `phoneList` component, along with its associated controller and template
-angular.
-  module('phoneList').
-  component('phoneList', {
-    templateUrl: 'phone-list/phone-list.template.html',
-    controller: function PhoneListController($http) {
-      var self = this;
-      self.orderProp = 'age';
+function PhoneListController($http) {
+  var self = this;
+  self.orderProp = 'age';
 
-      $http.get('phones/phones.json').then(function(response) {
-        self.phones = response.data;
-      });
-    }
+  $http.get('phones/phones.json').then(function (response) {
+    self.phones = response.data;
   });
+}
+
+//Create an $inject property on the controller function which holds an array of strings.
+// Each string in the array is the name of the service to inject for the corresponding parameter
+PhoneListController.$inject = ['$http'];
+
+// Register `phoneList` component, along with its associated controller and template
+angular.module('phoneList').component('phoneList', {
+  templateUrl: 'phone-list/phone-list.template.html',
+  controller: PhoneListController
+});
