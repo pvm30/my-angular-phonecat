a844020b60160b23c07462192adf92ef714cc753
Pedro Vázquez Machado <pedro.vazquez.machado@everis.com>
2018-09-21 13:08:46 +0200

    Solving problems with minification. Option 2)...

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -5,13 +5,14 @@ angular.module('phoneList').component('phoneList', {
   templateUrl: 'phone-list/phone-list.template.html',
   // Use an inline annotation where, instead of just providing the function, you provide an array.
   // This array contains a list of the service names, followed by the function itself as the last item of the array
-  controller: ['$http', function PhoneListController($http) {
-    var self = this;
-    self.orderProp = 'age';
+  controller: ['$http',
+    function PhoneListController($http) {
+      var self = this;
+      self.orderProp = 'age';
 
-    $http.get('phones/phones.json').then(function (response) {
-      self.phones = response.data;
-    });
-  }
+      $http.get('phones/phones.json').then(function (response) {
+        self.phones = response.data;
+      });
+    }
   ]
 });
